03d571634dfbb8a34c6f100ad3e72b9ccf079390
aneebiqbal <1aneebiqbal@gmail.com>
2021-11-19 23:06:08 +0500

    my Employee Services 1t commit

---
+++ EmployeeServices/Controllers/EmployeeController.cs
@@ -0,0 +1,110 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeServices.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get(string gnder = "All")
+        {
+            using (EmployeeDbEntities entities = new EmployeeDbEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDbEntities entities = new EmployeeDbEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+        
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDbEntities entities = new EmployeeDbEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+               return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+            }
+            
+        } 
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDbEntities entities = new EmployeeDbEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee with id  =  " + id.ToString() + " not found to delete");
+                    }
+                    else
+                    {
+
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }            
+        }
+        public HttpResponseMessage Put (int id, [FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDbEntities entities = new EmployeeDbEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "employee qith id " + id.ToString() + "not found to update");
+                    }
+                    else
+                    {
+
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+        }
+    }
+}
